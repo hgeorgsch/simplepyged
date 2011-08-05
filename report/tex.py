6b0ffa17f9bc2f4585d0f846fc7f4283f4050de5
Hans Georg Schaathun <georg@schaathun.net>
2011-08-05 14:12:27 +0200

    Attempted completion of the LaTeX builder.  Not tested.

--- report/tex.py
+++ report/tex.py
@@ -1,4 +1,34 @@
 # $Id$
 
 class texBuilder(object):
-   pass
+   def put_url(self,url,text="link"): 
+      print "\href{%s}{%s}" % (url,text,)
+   def put_cite(self,ref): 
+      print "\cite{%s}" % (ref,)
+   def put_name(self,fn,sn,ref=None): 
+      if ref == None: print "%s \textsc{%s}" % (fn,sn,),
+      else: print "%s \textsc{%s} (\textsc{%s})" % (fn,sn,ref),
+   def put_phead(self,fn,sn,no,key): 
+      print "\PersonHeader{%s}{%s %s}{%s}" % (no,fn,sn,key)
+   def put_phead_repeat(self,fn,sn,no,ref): 
+      print "\PersonRepeat{%s}{%s}{%s %s}" % (ref,no,fn,sn)
+   def put_subheader(self,header): print "\paragraph{%s}" %s (header,)
+   def put_shead_s(self): print "\section{",
+   def put_shead_e(self): print "}"
+   def put_chead_s(self): print "\chapter{",
+   def put_chead_e(self): print "}"
+   def put_item_s(self): print "  \item ",
+   def put_item_e(self): print
+   def put_enum_s(self): print "\begin{enumerate}\n",
+   def put_enum_e(self): print "\end{enumerate}\n"
+   def put_quot_s(self): print "\begin{quotation}\n«",
+   def put_quot_e(self): print "\end{quotation}\n»"
+   def put_foot_s(self): print "\footnote{",
+   def put_foot_e(self): print "}"
+   def put_dagger(self):
+      print "$\dagger"
+   def put_paragraph(self):
+      print "\n\n"
+   def put(self,x):
+      # TODO: escape
+      print x,
