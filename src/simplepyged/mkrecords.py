77cc9ac8e5ed52b6cf06826d645237ee4e86d318
Hans Georg Schaathun <hg+slekt@schaathun.net>
2012-08-16 19:39:29 +0100

    Added the merge feature in mkrecords.  Not tested.

--- src/simplepyged/mkrecords.py
+++ src/simplepyged/mkrecords.py
@@ -126,6 +126,11 @@ def parse_desc(file,dict,*a,**kw):
       if no in [ "source", "page", "subm" ]:
          kw[no] = line.strip()
 	 continue
+      if no == "merge":
+	 L = [ dict.get(l.strip()) for l in line.split() ]
+	 for n in L[1:]:
+	    L[0].merge(L[1:])
+	 continue
       if no == "g":
 	 # Get date of marriage
 	 (md,line) = line.split(";",1)
