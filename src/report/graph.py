3a3210f0576e487aa6aa411a85c0c95c089eae76
Hans Georg Schaathun <hg+slekt@schaathun.net>
2019-08-18 09:32:53 +0200

    Drafted family tree graph report

---
+++ src/report/graph.py
@@ -0,0 +1,80 @@
+#-*- coding: utf-8 -*-
+# $Id: report.py 768 2011-08-01 19:09:45Z georg $
+
+"""
+Graph generators for GEDCOM files.
+"""
+
+__all__ = [ "Graph" ]
+from . import date 
+from report import devnull, unsupport, IndiBins, dic_norsk, Builder
+from simplepyged.errors import * 
+from simplepyged.events import Event
+from simplepyged.records import parse_name
+
+from Queue import Queue
+
+graphpreamble = (u"\\documentclass{standalone}\n"
+              + u"\\usepackage{tikz}\n"
+              + u"\\usetikzlibrary{graphs,graphdrawing,quotes}\n"
+              + u"\\usegdlibrary{force}\n"
+              + u"\\begin{document}\n"
+              + u"  \\begin{tikzpicture}\n")
+graphpostamble = u"  \\end{tikzpicture}\n\\end{document}\n"
+
+
+
+class Graph(object):
+   __indicontext = True
+
+   def __init__(self,file,builder=None,dic=dic_norsk):
+      self.__file = file
+      self.__history = {}
+      self.__reflist = set()
+      self.__context = []
+      if builder == None: self._builder = Builder()
+      else: self._builder = builder
+      self._dic = dic
+
+   def list(self,q,header=None,abstract=None):
+      "Generate a report of a given list (queue) of individuals."
+
+      no = 0
+      ln = 999999
+
+      self._builder.preamble( graphpreamble )
+      self._builder.put( "\\graph[spring layout] {\n" )
+
+      # Main loop
+      while not q.empty():
+          pn = ln
+	  (ln, ind ) = q.get(False)
+          no += 1
+          print no, ln, ind.name()
+          if ln <= pn:
+              self._builder.put( ",\n" ) 
+          else:
+              self._builder.put( " -> " ) 
+          self._builder.put( '"\\parbox{16mm}{' ) 
+          self.simplename(ind) 
+          self._builder.put( '}"' ) 
+
+          self.history_add(ind,no)
+	  self.individual(ind=ind,number=no)
+
+      # Tail matter
+      self._builder.put( "}; \n" )
+      self._builder.postamble(graphpostamble)
+
+   def simplename(self,node):
+      (f,s) = node.name()
+      self._builder.put_name(f,s,ref)
+      by = node.birth_year()
+      dy = node.death_year()
+      if by < 0 and dy < 0: return
+      r = "("
+      if by >= 0: r +=str(by)
+      r += "--" 
+      if dy >= 0: r += str(dy) 
+      self._builder.put( r + ")" )
+
