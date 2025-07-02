bc122819fec036c537ce1cef954c5e1c655df9ad
Hans Georg Schaathun <georg@schaathun.net>
2025-07-02 10:19:38 +0200

    docstring typo

--- src/pyged/report/__init__.py
+++ src/pyged/report/__init__.py
@@ -3,7 +3,7 @@
 """
 Gedcom Reporting API
 
-This package provides an API produce reports from GEDCOM files.
+This package provides an API to produce reports from GEDCOM files.
 """
 
 from .report import Report
